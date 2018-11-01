2fd27b42c5a787de2fbceb9427f9969425e28af7
David An <david.d.an@MacBookPro2012.fios-router.home>
2018-11-01 13:29:09 -0400

    pylab added to show graphs

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import pylab
 from testCases_v2 import *
 import sklearn
 import sklearn.datasets
@@ -45,6 +46,7 @@ def faulty_main():
     print (
         'Accuracy of logistic regression: %d ' % accuracy +
         '% ' + "(percentage of correctly labelled datapoints)")
+    pylab.show()
 
 ########################################
 
@@ -393,6 +395,42 @@ def extrafunc():
     predictions = predict(parameters, X)
     print ('Accuracy: %d' % float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100) + '%')
 
+    plt.figure(figsize=(16, 32))
+    hidden_layer_sizes = [1, 2, 3, 4, 5, 20, 50]
+    for i, n_h in enumerate(hidden_layer_sizes):
+        plt.subplot(5, 2, i+1)
+        plt.title('Hidden Layer of size %d' % n_h)
+        parameters = nn_model(X, Y, n_h, num_iterations = 5000)
+        plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+        predictions = predict(parameters, X)
+        accuracy = float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100)
+        print ("Accuracy for {} hidden units: {} %".format(n_h, accuracy))
+
     # Plot the decision boundary
     plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
     plt.title("Decision Boundary for hidden layer size " + str(4))
+    pylab.show()
+
+def optionalfunc():
+    # Datasets
+    noisy_circles, noisy_moons, blobs, gaussian_quantiles, no_structure = load_extra_datasets()
+
+    datasets = {"noisy_circles": noisy_circles,
+                "noisy_moons": noisy_moons,
+                "blobs": blobs,
+                "gaussian_quantiles": gaussian_quantiles}
+
+    ### START CODE HERE ### (choose your dataset)
+    dataset = "noisy_moons"
+    ### END CODE HERE ###
+
+    X, Y = datasets[dataset]
+    X, Y = X.T, Y.reshape(1, Y.shape[0])
+
+    # make blobs binary
+    if dataset == "blobs":
+        Y = Y%2
+
+    # Visualize the data
+    plt.scatter(X[0, :], X[1, :], c=Y.ravel().tolist(), s=40, cmap=plt.cm.get_cmap("Spectral"))
+    pylab.show()
