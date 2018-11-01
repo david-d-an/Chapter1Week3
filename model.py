1778dff504f75251d086bd79b7682fe303a304cd
David An <david.d.an@MacBookPro2012.fios-router.home>
2018-11-01 10:56:01 -0400

    fixed bugs from Coursera connection

--- model.py
+++ model.py
@@ -33,15 +33,17 @@ def faulty_main():
 
     # Train the logistic regression classifier
     clf = sklearn.linear_model.LogisticRegressionCV()
-    clf.fit(X.T, Y.T)
+    clf.fit(X.T, Y.T.ravel())
 
     # Plot the decision boundary for logistic regression
     plot_decision_boundary(lambda x: clf.predict(x), X, Y)
     plt.title("Logistic Regression")
 
-    # Print accuracy
+    # # Print accuracy
     LR_predictions = clf.predict(X.T)
-    print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+    accuracy = float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100)
+    print (
+        'Accuracy of logistic regression: %d ' % accuracy +
         '% ' + "(percentage of correctly labelled datapoints)")
 
 ########################################
